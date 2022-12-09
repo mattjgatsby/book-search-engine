d25bb1fcdc7d4decd661b938ec1b2cacd126ed6a
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-08 22:19:21 -0500

    updated user type def, added type book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -59,3 +59,5 @@ const resolvers = {
     },
   },
 };
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,26 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
 type User {
-    _id: ID!
-    username: String!
-    email: String!
-    password: String!
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
 
+type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
 type Query {
     user: [User]
 }
 }
-`
+`;
 
 module.exports = typeDefs;
