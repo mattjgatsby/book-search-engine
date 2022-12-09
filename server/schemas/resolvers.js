9010fa9ba2281c91e3c9338edc15e0076c13c43d
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-08 22:11:05 -0500

    wrote out adduser, loginUser, and saveBook mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,26 +1,48 @@
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
-const { AuthenticationError } = require('apollo-server-express');
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
-    Query: {
-        getAllUsers: async () => {
-            return User.find();
-        },
-        me: async (parent, args, context) => {
-            const foundUser = await User.findById(context.user._id);
-
-            if (!foundUser) {
-                throw new AuthenticationError('Cannot find a user with this id!');
-            }
-
-            return foundUser;
-        }
+  Query: {
+    getAllUsers: async () => {
+      return User.find();
     },
+    me: async (parent, args, context) => {
+      const foundUser = await User.findById(context.user._id);
+
+      if (!foundUser) {
+        throw new AuthenticationError("No user with this id");
+      }
+
+      return foundUser;
+    },
+  },
   Mutation: {
-    createUser: async (parent, args) => {
-        const newUser = await User.create(args);
-        return newUser;
-    }
-  }
+    addUser: async (parent, { username, email, password }) => {
+      return User.create({ username, email, password });
+    },
+    loginUser: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Not a valid user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book } },
+        { new: true, runValidators: true }
+      );
+
+      return updatedUser;
+    },
+  },
 };
