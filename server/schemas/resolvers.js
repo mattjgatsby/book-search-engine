e6f77a38f8b6208f2572ee55ac3c2d583ec04344
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-08 22:00:39 -0500

    modified query in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,22 @@
-const { User } = require("../models");
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
-  Query: {
-    user: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return User.find(params);
+    Query: {
+        getAllUsers: async () => {
+            return User.find();
+        },
+        me: async (parent, args, context) => {
+            const foundUser = await User.findById(context.user._id);
+
+            if (!foundUser) {
+                throw new AuthenticationError('Cannot find a user with this id!');
+            }
+
+            return foundUser;
+        }
     },
-  },
   Mutation: {
     createUser: async (parent, args) => {
         const newUser = await User.create(args);
